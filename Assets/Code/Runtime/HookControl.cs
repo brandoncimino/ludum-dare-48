65b6041da08746db1e2d0232d199b46383260af9
GuyWithPasta <guywithpasta@gmail.com>
2021-04-24 12:20:07 -0500

    Moved HookControl to proper Code Subfolder

---
+++ Assets/Code/Runtime/HookControl.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class HookControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+         var keyboard = Keyboard.current;
+         if (keyboard == null)
+         {
+             Debug.LogError("No Keyboard Detected");
+             return; //no keyboard detected
+         }
+
+
+
+         transform.position += DirectInputToVector3(keyboard);
+    }
+
+    /// <summary>
+    /// Given the current buttons pushed within WASD, return a Vector2 where A&D control left and right, and W&S control up and down respectively.
+    /// </summary>
+    /// <param name="keyboard">Input Manager's keyboard. This is the current keyboard</param>
+    /// <returns>A Normalized Vector2 based on WASD input</returns>
+    Vector3 DirectInputToVector3(Keyboard keyboard)
+    {
+        return new Vector2(keyboard.dKey.ReadValue() - keyboard.aKey.ReadValue(),
+            keyboard.wKey.ReadValue() - keyboard.sKey.ReadValue());
+    }
+
+    public void HookMover(InputAction.CallbackContext context)
+    {
+        Debug.Log("Indirect Input Detected");
+        
+    }
+    
+}
