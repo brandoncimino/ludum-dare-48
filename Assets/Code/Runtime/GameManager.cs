a52aba8ed73e178039b85dd98647c27a7815a78a
brandoncimino <brandon.cimino@gmail.com>
2021-04-26 15:27:58 -0500

    Refactored GameManager.cs namespace

--- Assets/Code/Runtime/GameManager.cs
+++ Assets/Code/Runtime/GameManager.cs
@@ -1,44 +1,32 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
-{
-    public static GameManager Single;
-    public bool isGameOver = false;
-    
-    private void Awake()
-    {
-        Single = this;
-    }
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        // subscribe to the event manager
-        EventManager.Single.ONTriggerFirstCatch     += DecideGameOver;
-        // in the min. version the game is over on the first catch
-    }
-    
-    protected void OnDestroy() {
-        
-        // unsubscribe from the event manager
-        EventManager.Single.ONTriggerFirstCatch     -= DecideGameOver;
-    }
+namespace Code.Runtime {
+    public class GameManager : MonoBehaviour {
+        public static GameManager Single;
+        public        bool        isGameOver = false;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+        private void Awake() {
+            Single = this;
+        }
 
-    private void DecideGameOver()
-    {
-        isGameOver = true;
-        EventManager.Single.TriggerGameOver();
-        
-    }
+        // Start is called before the first frame update
+        void Start() {
+            // subscribe to the event manager
+            EventManager.Single.ONTriggerFirstCatch += DecideGameOver;
+            // in the min. version the game is over on the first catch
+        }
 
-    
+        protected void OnDestroy() {
+            // unsubscribe from the event manager
+            EventManager.Single.ONTriggerFirstCatch -= DecideGameOver;
+        }
+
+        // Update is called once per frame
+        void Update() { }
+
+        private void DecideGameOver() {
+            isGameOver = true;
+            EventManager.Single.TriggerGameOver();
+        }
+    }
 }
