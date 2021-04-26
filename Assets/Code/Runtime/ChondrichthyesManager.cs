eb7183c7ce9a8f92ad22659ce1509944d74c0d34
Michael <michael.b.rawls@gmail.com>
2021-04-26 18:23:52 -0500

    Added ChondrichthyesManager to spawn fish at intervals.

---
+++ Assets/Code/Runtime/ChondrichthyesManager.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms;
+
+public class ChondrichthyesManager : MonoBehaviour
+{
+    public Camera cameraCrew;
+    public GameObject goblinShark;
+    public GameObject basicFish;
+
+    public float minimumTimeBeforeNextFish;
+    public float maxiumumTimeBeforeNextFish;
+    public float distanceFromCameraCrewToSpawnFish;
+    [Range(0, 100)] public float chanceToUseGoblinShark;
+
+    public int gameLevel;
+
+    private float lastFishTimeStamp;
+    private float nextFishDelivery;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastFishTimeStamp = 0f;
+        GenerateFishTicket();
+    }
+
+    /// <summary>
+    /// Determine when the next fish will be spawned in
+    /// </summary>
+    private void GenerateFishTicket()
+    {
+        nextFishDelivery = Time.time + Random.Range(minimumTimeBeforeNextFish, maxiumumTimeBeforeNextFish);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time >= nextFishDelivery)
+        {
+            GenerateFishTicket();
+            SpawnFish();
+        }
+    }
+
+    /// <summary>
+    /// Given camera position and game level, determine location ahead of hook and instantiate fish
+    /// </summary>
+    private void SpawnFish()
+    {
+        var ray = cameraCrew.ScreenPointToRay(new Vector3(Random.Range(0, Screen.width), Random.Range(0, Screen.height), 0));
+        Instantiate(RandomFish(), ray.GetPoint(distanceFromCameraCrewToSpawnFish), Random.rotation);
+    }
+
+    private GameObject RandomFish()
+    {
+        if (gameLevel >= 1 && Random.Range(0, 100) >= chanceToUseGoblinShark)
+        {
+            return goblinShark;
+        }
+        return basicFish;
+    }
+}
