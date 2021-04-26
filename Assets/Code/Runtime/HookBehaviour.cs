259577906007ec1aa5f7e7968384a478730d1e41
brandoncimino <brandon.cimino@gmail.com>
2021-04-25 23:10:15 -0500

    Fixed namespace in HookBehaviour.cs Removed generated code for HookActions because it turns out the c# code doesn't do anything and shouldn't be used (based on comments from unity devs on the forums)

--- Assets/Code/Runtime/HookBehaviour.cs
+++ Assets/Code/Runtime/HookBehaviour.cs
@@ -1,97 +1,107 @@
 using System.Collections.Generic;
 
-using Code.Runtime;
-
 using UnityEngine;
 
-public class HookBehaviour : MonoBehaviour
-{
-    public float test = 0f;
-    public static HookBehaviour Single;
-    public        Rigidbody     MyRigidbody;
-    public Rigidbody MouthCollector;
+namespace Code.Runtime {
+    public class HookBehaviour : MonoBehaviour {
+        public        float         test = 0f;
+        public static HookBehaviour Single;
+        public        Rigidbody     MyRigidbody;
+        public        Rigidbody     MouthCollector;
 
-    private float _depth;
-    private float _pressure;
-    private float _depthInitial;
+        private float _depth;
+        private float _pressure;
+        private float _depthInitial;
 
-    private Vector3 _velocityPull  = new Vector3(0, 0, 0);
-    private Vector3 _velocityPush  = new Vector3(0, 0, 0);
-    private Vector3 _directionPush = new Vector3(0, 0, 0);
-    private float   _pullModifier  = 0.001f;
-    private float   _pushModifier  = 0.001f;
-    private float   _maxDepth      = 1000f;
+        private Vector3 _velocityPull  = new Vector3(0, 0, 0);
+        private Vector3 _velocityPush  = new Vector3(0, 0, 0);
+        private Vector3 _directionPush = new Vector3(0, 0, 0);
+        private float   _pullModifier  = 0.001f;
+        private float   _pushModifier  = 0.001f;
+        private float   _maxDepth      = 1000f;
 
-    public List<Catchables> myCatches;
+        private Quaternion _rotationInitial;
+        public  float      StabilizerSmoothness;
 
-    private void Awake() {
-        Single = this;
-    }
+        public List<Catchables> myCatches;
 
-    // Start is called before the first frame update
-    private void Start() {
-        _depthInitial = transform.localPosition.y;
-
-        // substrictions to the event manager
-        EventManager.Single.ONTriggerCollisionFish   += CollisionFish;
-        EventManager.Single.ONTriggerCollisionDebris += CollisionDebris;
-        EventManager.Single.ONTriggerCollisionCatchable += CollisionCatchable;
-    }
-
-    private void OnDestroy() {
-        // cancel all substrictions to the event manager
-        EventManager.Single.ONTriggerCollisionFish   -= CollisionFish;
-        EventManager.Single.ONTriggerCollisionDebris += CollisionDebris;
-        EventManager.Single.ONTriggerCollisionCatchable -= CollisionCatchable;
-    }
-
-
-    // Update is called once per frame
-    void Update() {
-        // falling down based on pressure and gravity
-        _depth          = Mathf.Abs(transform.localPosition.y - _depthInitial);
-        _velocityPull.y = WaterManager.Single.computePull(_depth, _maxDepth);
-
-        // user induced movement
-        // TODO: David, he knows how the input system works
-
-        // movement in total (additive as an approximation)
-        var moveVector = (_pullModifier * _velocityPull + _pushModifier * _velocityPush);
-        MyRigidbody.velocity = moveVector;
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        test = 1;
-        if (other.GetComponent<Catchables>() != null)
-        {
-            test = 2;
-            EventManager.Single.TriggerCollisionCatchable(other.GetComponent<Catchables>());
-        }
-    }
-
-    private void CollisionFish(FishBehaviour fish) {
-        // fish-specific collision stuff
-    }
-
-    private void CollisionDebris(DebrisBehaviour debris) {
-        // debris-specific collision stuff
-    }
-
-    private void CollisionCatchable(Catchables newCatch)
-    {
-        test = 3;
-        // general reaction to having caught a catchable: Yay!
-        if (myCatches.Count == 0) {
-            EventManager.Single.TriggerFirstCatch();
+        private void Awake() {
+            Single = this;
         }
 
-        myCatches.Add(newCatch);
-    }
+        // Start is called before the first frame update
+        private void Start() {
+            var transform1 = transform;
+            _depthInitial    = transform1.localPosition.y;
+            _rotationInitial = transform1.rotation;
 
-    public Rigidbody FindHook()
-    {
-        return MouthCollector;
-    }
+            // substrictions to the event manager
+            EventManager.Single.ONTriggerCollisionFish      += CollisionFish;
+            EventManager.Single.ONTriggerCollisionDebris    += CollisionDebris;
+            EventManager.Single.ONTriggerCollisionCatchable += CollisionCatchable;
+        }
 
+        private void OnDestroy() {
+            // cancel all substrictions to the event manager
+            EventManager.Single.ONTriggerCollisionFish      -= CollisionFish;
+            EventManager.Single.ONTriggerCollisionDebris    += CollisionDebris;
+            EventManager.Single.ONTriggerCollisionCatchable -= CollisionCatchable;
+        }
+
+
+        // Update is called once per frame
+        void Update() {
+            // falling down based on pressure and gravity
+            _depth          = Mathf.Abs(transform.localPosition.y - _depthInitial);
+            _velocityPull.y = WaterManager.Single.computePull(_depth, _maxDepth);
+
+            // user induced movement
+            // TODO: David, he knows how the input system works
+
+            // movement in total (additive as an approximation)
+            var moveVector = (_pullModifier * _velocityPull + _pushModifier * _velocityPush);
+            MyRigidbody.velocity = moveVector;
+        }
+
+        private void OnTriggerEnter(Collider other) {
+            test = 1;
+            if (other.GetComponent<Catchables>() != null) {
+                test = 2;
+                EventManager.Single.TriggerCollisionCatchable(other.GetComponent<Catchables>());
+            }
+        }
+
+        private void CollisionFish(FishBehaviour fish) {
+            // fish-specific collision stuff
+        }
+
+        private void CollisionDebris(DebrisBehaviour debris) {
+            // debris-specific collision stuff
+        }
+
+        private void CollisionCatchable(Catchables newCatch) {
+            test = 3;
+            // general reaction to having caught a catchable: Yay!
+            if (myCatches.Count == 0) {
+                EventManager.Single.TriggerFirstCatch();
+            }
+
+            myCatches.Add(newCatch);
+        }
+
+        public Rigidbody FindHook() {
+            return MouthCollector;
+        }
+
+        private void ApplyStabilization() {
+            // Calculate the desired rotation, which will incorporate a bit of wobble
+            var targetRotation = new Vector3(
+                0,
+                90,
+                0
+            );
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, _rotationInitial, Time.deltaTime * StabilizerSmoothness);
+        }
+    }
 }
