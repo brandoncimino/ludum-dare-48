604b6ff43506c963826c185d6199d6dfbb6f98df
nicole <nicole.aretz@t-online.de>
2021-04-27 03:54:47 +0200

    destroyed the fish zen by teaching individualism (randem variables in fish behaviour)

--- Assets/Code/Runtime/FishBehaviour.cs
+++ Assets/Code/Runtime/FishBehaviour.cs
@@ -1,3 +1,4 @@
+using System.Transactions;
 using Code.Runtime;
 using JetBrains.Annotations;
 using TMPro;
@@ -8,34 +9,43 @@ public class FishBehaviour : Catchables {
     public Collider myInnerCollider;
 
     protected float speed = 5;
+    protected float fleeAngle = 90f;
+    protected float fleeTime = 1f;
 
     //protected Vector3 direction = Vector3.forward;
 
     public bool isUncomfortable = false;
 
     protected float timeTillChange    = 4f;
-    protected float minTimeTillChange = 1f;
+    protected float minTimeTillChange = 3f;
     protected float maxTimeTillChange = 7f;
 
     protected float timeForChange    = 0.1f;
     protected float minTimeForChange = 0.2f;
-    protected float maxTimeForChange = 1f;
+    protected float maxTimeForChange = 0.4f;
     protected float AngleChange      = 90;
 
     public TMP_Text thoughts;
 
     protected override void myStartBehaviour() {
         timeTillChange = Random.Range(minTimeTillChange, maxTimeTillChange);
+        AngleChange = Random.Range(-90f, 90f);
     }
 
     // Update is called once per frame
     protected virtual void Update()
     {
+        
         // change direction if feeling uncomfortable
-        if (isUncomfortable) {
-            // always avoid in the right direction
-            var angle = 90 * Time.deltaTime;
-            transform.Rotate(Vector3.up, angle);
+        if (isUncomfortable)
+        {
+            fleeTime -= Time.deltaTime;
+            if (fleeTime < 0)
+            {
+                CalmDown();
+            }
+            
+            transform.Rotate(Vector3.up, fleeAngle * Time.deltaTime);
         }
         else {
             changeDirectionToHorizonntal();
@@ -43,18 +53,25 @@ public class FishBehaviour : Catchables {
         }
 
         // move forward
-        catchableRigidbody.velocity = speed * transform.forward;
+        catchableRigidbody.velocity = (speed + Mathf.Sin(timeForChange + timeTillChange)) * transform.forward;
     }
 
 
     public virtual void Scare([CanBeNull] Transform enemy = null) {
         isUncomfortable = true;
-        thoughts.text   = "AAAAAAAAAAAAAAAAAAAAAAAAAH";
+        thoughts.text   = "AAAAAH";
+        fleeAngle = Random.Range(10f, 90f);
     }
 
     public virtual void CalmDown([CanBeNull] Transform enemy = null) {
         isUncomfortable = false;
         thoughts.text   = "that was close";
+
+        fleeTime = Random.Range(1f, 2f);
+        
+        timeTillChange = Random.Range(minTimeTillChange, maxTimeTillChange);
+        timeForChange  = Random.Range(minTimeForChange,  maxTimeForChange);
+        AngleChange    = Random.Range(-90f,              90f);
     }
 
 
