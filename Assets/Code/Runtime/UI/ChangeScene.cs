064322e02c0bd7c05c42ba116e364c07fc142657
nicole <nicole.aretz@t-online.de>
2021-04-27 19:11:40 +0200

    music in the title screen

--- Assets/Code/Runtime/UI/ChangeScene.cs
+++ Assets/Code/Runtime/UI/ChangeScene.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,8 +6,20 @@ using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour
 {
+    public AudioSource clickEffectStart;
+    private float SceneChangeTime = -1f;
+    private bool shallChange = false;
+
+    private void Update()
+    {
+        if (SceneChangeTime > 0) SceneChangeTime -= Time.deltaTime;
+        if (shallChange && Mathf.Abs(SceneChangeTime) < 1e-1) SceneManager.LoadScene("GoFish");
+    }
+
     public void buttonStart()
     {
-        SceneManager.LoadScene("GoFish");
+        shallChange = true;
+        SceneChangeTime = 1.5f;
+        clickEffectStart.Play();
     }
 }
