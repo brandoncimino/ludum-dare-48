5025b38db868da7cf2692b66f2683a729eb1c1e7
brandoncimino <brandon.cimino@gmail.com>
2021-04-29 23:10:41 -0500

    Began adding the Arborist.cs

---
+++ Assets/Code/Runtime/Bathymetry/Arborist.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using BrandonUtils.Standalone.Attributes;
+
+using UnityEngine;
+
+namespace Code.Runtime.Bathymetry {
+    public class Arborist : MonoBehaviour {
+        public Terrain Arboretum;
+
+        [EditorInvocationButton]
+        public void Deforest() {
+            Arboretum.terrainData.SetTreeInstances(Array.Empty<TreeInstance>(), false);
+        }
+
+        [EditorInvocationButton]
+        public void Weed() {
+            throw new NotImplementedException("I don't know how to remove the terrain details yet");
+        }
+    }
+}
